09d1d33a21835f30b64f6e6e02cdcaf9cbb90ff1
JBriolotti <jbriolotti@gmail.com>
2024-11-26 09:20:48 -0300

    Update 07.js

--- C6 Bucles/ejercicios/07.js
+++ C6 Bucles/ejercicios/07.js
@@ -3,6 +3,14 @@ function esParYDivisiblePorTres(a) {
   // Retorna true si este es par y divisible por tres a la vez.
   // Retorna false si no lo es.
   // Tu código:
+  // Verificamos si "a" es par y divisible por 3
+  if (a % 2 === 0 && a % 3 === 0) {
+    return true;
+  }
+  return false;
 }
 
-module.exports = esParYDivisiblePorTres;
+module.exports = esParYDivisiblePorTres; // 
+}
+
+module.exports = esParYDivisiblePorTres; //
