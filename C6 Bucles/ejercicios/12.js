8ec1196592887e91bf7ccbbe43f08fa6755ad8ae
JBriolotti <jbriolotti@gmail.com>
2024-11-26 09:41:59 -0300

    Update 12.js

--- C6 Bucles/ejercicios/12.js
+++ C6 Bucles/ejercicios/12.js
@@ -4,6 +4,23 @@ function fizzBuzz(num) {
   // Si "num" es divisible entre 3 y 5 (ambos), retorna "fizzbuzz".
   // De lo contrario, retorna false.
   // Tu código:
+  // Verificamos si es divisible entre 3 y 5
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "fizzbuzz";
+  }
+  // Verificamos si es divisible solo entre 3
+  if (num % 3 === 0) {
+    return "fizz";
+  }
+  // Verificamos si es divisible solo entre 5
+  if (num % 5 === 0) {
+    return "buzz";
+  }
+  // Si no es divisible ni entre 3 ni entre 5, retornamos false
+  return false;
+}
+
+module.exports = fizzBuzz;
 }
 
 module.exports = fizzBuzz;
