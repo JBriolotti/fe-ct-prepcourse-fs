eec67b709a2748f8b5642685e099ff3dae1718fd
JBriolotti <jbriolotti@gmail.com>
2024-11-26 09:52:10 -0300

    Update 18.js

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,21 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  // Inicializamos el resultado en 1
+  let producto = 1;
+
+  // Aseguramos que a sea menor o igual a b
+  if (a > b) {
+    // Si a es mayor que b, intercambiamos los valores
+    [a, b] = [b, a];
+  }
+
+  // Multiplicamos todos los números en el rango de a a b
+  for (let i = a; i <= b; i++) {
+    producto *= i;
+  }
+
+  return producto;
 }
 
 module.exports = productoEntreNúmeros;
