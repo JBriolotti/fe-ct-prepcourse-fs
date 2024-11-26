13c2b469b6c2f8ba7479bf7c8e215daadb165530
JBriolotti <jbriolotti@gmail.com>
2024-11-26 09:38:16 -0300

    Update 11.js

--- C6 Bucles/ejercicios/11.js
+++ C6 Bucles/ejercicios/11.js
@@ -2,6 +2,12 @@ function estaEnRango(num) {
   // Retorna true si "num" es menor que 50 y mayor que 20.
   // De lo contrario, retornar false.
   // Tu código:
+   // Verificamos si el número está en el rango entre 20 y 50
+  if (num > 20 && num < 50) {
+    return true;
+  }
+  return false;
 }
-
+module.exports = estaEnRango;
+}
 module.exports = estaEnRango;
