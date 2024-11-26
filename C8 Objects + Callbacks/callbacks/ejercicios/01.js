35db0f4ef82fdc8e7413010f4258391632e83700
JBriolotti <jbriolotti@gmail.com>
2024-11-26 16:16:28 -0300

    Update 01.js

--- C8 Objects + Callbacks/callbacks/ejercicios/01.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/01.js
@@ -2,6 +2,9 @@ function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // NOTA: no debes retornar nada.
    // Tu código:
+   function invocarCallback(cb) {
+   // Invoca/ejecuta el callback `cb`
+   cb();
 }
-
 module.exports = invocarCallback;
+
