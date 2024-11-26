fd364a8bee0d5543b7c345e9ed2e2b0642168b98
JBriolotti <jbriolotti@gmail.com>
2024-11-26 16:25:14 -0300

    Update 08.js

--- C8 Objects + Callbacks/callbacks/ejercicios/08.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/08.js
@@ -3,6 +3,11 @@ const buscarElemento = (array, callback) => {
   // Si el elemento no se encuentra, devuelve el mensje "No se encontró el elemento".
   // La función de callback es la encargada de evaluar si el elemento fue encontrado.
   // Tu código:
+  // Usamos find para encontrar el primer elemento que cumpla con la condición del callback
+  const elemento = array.find(callback);
+
+  // Si se encontró un elemento, lo retornamos. Si no, devolvemos el mensaje de error.
+  return elemento ? elemento : "No se encontró el elemento";
 };
 
 module.exports = buscarElemento;
