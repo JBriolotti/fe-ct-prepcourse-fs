eb99a129f0778e65537b149861ca239c12928741
JBriolotti <jbriolotti@gmail.com>
2024-11-26 10:00:16 -0300

    Update 21.js

--- C6 Bucles/ejercicios/21.js
+++ C6 Bucles/ejercicios/21.js
@@ -4,6 +4,18 @@ function esPotenciaDeDos(numero) {
   // Devuelve true si lo es, sino devuelve false.
   // PISTA: Utiliza un bucle while.
   // Tu código:
+   if (numero <= 0) {
+    return false; // Los números negativos o cero no pueden ser potencias de 2
+  }
+  
+  while (numero > 1) {
+    if (numero % 2 !== 0) {
+      return false; // Si no es divisible por 2, no es potencia de 2
+    }
+    numero = numero / 2;
+  }
+  
+  return true; // Si llegamos a 1, entonces es una potencia de 2
 }
 
 module.exports = esPotenciaDeDos;
