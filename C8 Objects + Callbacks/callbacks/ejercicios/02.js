3a20192e8a898de8c8aee0a4813ed0b0b39c248d
JBriolotti <jbriolotti@gmail.com>
2024-11-26 16:17:56 -0300

    Update 02.js

--- C8 Objects + Callbacks/callbacks/ejercicios/02.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/02.js
@@ -3,6 +3,12 @@ function cambiarCadena(string, callback) {
    // La función de callback se encargará de recibir el string y devolverlo con los cambios.
    // Si no se recibe una función callback entonces se debe retornar el string original.
    // Tu código:
+   function cambiarCadena(string, callback) {
+   // Verificamos si se ha proporcionado un callback y si es una función
+   if (typeof callback === "function") {
+      return callback(string); // Aplica la función callback al string
+   }
+   return string; // Si no hay callback, retornamos el string original
 }
 
 module.exports = cambiarCadena;
