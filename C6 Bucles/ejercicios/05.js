9a182fd40ab4fe3c4c7604cfc9eea23aea06a1c5
JBriolotti <jbriolotti@gmail.com>
2024-11-26 09:15:30 -0300

    Update 05.js

--- C6 Bucles/ejercicios/05.js
+++ C6 Bucles/ejercicios/05.js
@@ -2,6 +2,15 @@ function esVerdadero(valor) {
   // Si "valor" es verdadero, retorna "Soy verdadero".
   // Caso contrario, retorna "Soy falso".
   // Tu código:
+    // Verificamos si el valor es verdadero
+  if (valor) {
+    return "Soy verdadero";
+  } else {
+    return "Soy falso";
+  }
+}
+
+module.exports = esVerdadero;
 }
 
 module.exports = esVerdadero;
