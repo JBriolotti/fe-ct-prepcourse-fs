33f3eb8055427f20b8ffd80289fda1fca5667037
JBriolotti <jbriolotti@gmail.com>
2024-11-26 09:14:08 -0300

    Update 04.js

--- C6 Bucles/ejercicios/04.js
+++ C6 Bucles/ejercicios/04.js
@@ -3,6 +3,15 @@ function mayoriaDeEdad(edad) {
   // Si tiene 18 años o más, retorna el string: "Allowed".
   // Caso contrario: "Not allowed".
   // Tu código:
+   // Verificamos si la edad es 18 o mayor
+  if (edad >= 18) {
+    return "Allowed";
+  } else {
+    return "Not allowed";
+  }
+}
+
+module.exports = mayoriaDeEdad;
 }
 
 module.exports = mayoriaDeEdad;
