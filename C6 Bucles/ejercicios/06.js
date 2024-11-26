9408ed26ea05591f388c05951054f3b8c065a9b7
JBriolotti <jbriolotti@gmail.com>
2024-11-26 09:18:45 -0300

    Update 06.js

--- C6 Bucles/ejercicios/06.js
+++ C6 Bucles/ejercicios/06.js
@@ -2,6 +2,14 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+   // Verificamos si el número está en el rango de tres dígitos, ya sea positivo o negativo
+  if (num >= 100 && num <= 999 || num <= -100 && num >= -999) {
+    return true;
+  }
+  return false;
+}
+
+module.exports = tieneTresDigitos;
 }
 
 module.exports = tieneTresDigitos;
