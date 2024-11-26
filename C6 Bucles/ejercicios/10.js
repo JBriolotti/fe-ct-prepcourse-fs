57fa466ee9c423961d0370a732b846cab207b5f7
JBriolotti <jbriolotti@gmail.com>
2024-11-26 09:25:03 -0300

    Update 10.js

--- C6 Bucles/ejercicios/10.js
+++ C6 Bucles/ejercicios/10.js
@@ -2,6 +2,14 @@ function esDiezOCinco(num) {
   // Retornar true si "num" es 10 o 5.
   // De lo contrario, retornar false.
   // Tu código:
+   // Verificamos si el número es igual a 10 o 5
+  if (num === 10 || num === 5) {
+    return true;
+  }
+  return false;
+}
+
+module.exports = esDiezOCinco;
 }
 
 module.exports = esDiezOCinco;
