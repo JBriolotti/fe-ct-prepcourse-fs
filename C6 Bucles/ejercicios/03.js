85805d3cc59ca74624f3651d0ea18d82114b920f
JBriolotti <jbriolotti@gmail.com>
2024-11-26 09:13:02 -0300

    Update 03.js

--- C6 Bucles/ejercicios/03.js
+++ C6 Bucles/ejercicios/03.js
@@ -3,6 +3,15 @@ function obtenerMayor(x, y) {
   // Retorna el número más grande.
   // Si son iguales, retornar cualquiera de los dos.
   // Tu código:
+   // Retorna el número mayor entre "x" e "y"
+  if (x > y) {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+module.exports = obtenerMayor;    
 }
 
 module.exports = obtenerMayor;
