daebe2d9a85906b292a0a118177ae4dd8a2d9d19
JBriolotti <jbriolotti@gmail.com>
2024-11-26 16:21:47 -0300

    Update 04.js

--- C8 Objects + Callbacks/callbacks/ejercicios/04.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/04.js
@@ -4,6 +4,11 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // NOTA: no debes retornar nada.
    // Tu código:
+   function sumarArray(arrayOfNumbers, cb) {
+   // Sumar todos los números del arreglo
+   const suma = arrayOfNumbers.reduce((acc, num) => acc + num, 0);
+   
+   // Pasamos el resultado al callback
+   cb(suma);
 }
-
 module.exports = sumarArray;
