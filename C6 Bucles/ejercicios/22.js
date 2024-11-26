23d3321efa95343d9a358415a33d358f7dc0ffd8
JBriolotti <jbriolotti@gmail.com>
2024-11-26 10:01:22 -0300

    Update 22.js

--- C6 Bucles/ejercicios/22.js
+++ C6 Bucles/ejercicios/22.js
@@ -3,6 +3,14 @@ function doWhile(num) {
   // Retorna el valor final.
   // PISTA: Utiliza el bucle do-while.
   // Tu código:
+    let count = 0;
+  
+  do {
+    num += 5;  // Aumentamos num en 5
+    count++;    // Aumentamos el contador de iteraciones
+  } while (count < 8); // El bucle continúa hasta que se haya ejecutado 8 veces
+  
+  return num; // Retornamos el valor final de num
 }
 
 module.exports = doWhile;
